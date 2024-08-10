2d6daa6d3cea238329c2717218990cef7bd83a0e
msohaaib <msohaib968505@gmail.com>
2024-08-10 19:19:06 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from './componenets/Benefits'
 import Collaboration from './componenets/Collaboration'
 import Header from './componenets/Header'
 import Hero from './componenets/Hero'
+import Services from './componenets/Services'
 function App() {
   return (
     <>      
@@ -13,6 +14,7 @@ function App() {
       </div>
       <ButtonGradient />
       <Collaboration />
+      <Services />
     </>
   )
 }
